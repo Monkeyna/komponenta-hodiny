ebc3aef602f8d7554af5e9c0a9456188323a347d
Kristýna Mandíková <tyna.mandi@gmail.com>
2023-08-16 17:17:44 +0200

    hodiny js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+    
+
+
+const times = [
+    {
+      hours: 12,
+      minutes: 34,
+    },
+    {
+      hours: 5,
+      minutes: 23,
+    },
+    {
+      hours: 17,
+      minutes: 8,
+    },
+  ];
+  
+  const Clock = (props) => {
+    const { hours, minutes } = props;
+    const paddedHours = String(hours).padStart(2, '0');
+    const paddedMinutes = String(minutes).padStart(2, '0');
+  
+    return `
+      <div class="clock">
+        <span class="clock__hours">${paddedHours}</span>:<span class="clock__minutes">${paddedMinutes}</span>
+      </div>
+    `;
+  };
+  
+  const appElm = document.querySelector('#app');
+  appElm.innerHTML = times.map((time) => Clock(time)).join('');
+
+
